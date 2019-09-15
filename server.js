1ea016822ae1608c8b2431b25ebddb5c69dc1e6f
icepants <shawnluther99@gmail.com>
2019-09-15 17:40:17 -0400

    Added Initial Server Code.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const helpers = require('./helpers')
+// require routes
+const routeHome = require('./routes/home')
+const routeAbout = require('./routes/about')
+const app = express()
+// use express-handlebars view engine and set views template directory
+const hbs = exphbs.create({
+    partialsDir: __dirname + '/views/partials',
+    helpers: helpers()
+})
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+// serve static files form /public
+app.use(express.static(path.resolve(__dirname, 'public'))) // serve static files
+// Set your routes here
+app.get('/', (req, res, next) => routeHome(req, res, next))
+app.get('/about', (req, res, next) => routeAbout(req, res, next))
+
+// Start server.
+app.listen(PORT, function () {
+    // Let us know the server has started successfully.
+    console.log("Server listening on: http://localhost:" + PORT);
+});
