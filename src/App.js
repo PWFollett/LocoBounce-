0c450aa3ed4d84db2e022393c899731a6ddce404
PWFollett <pwf592110@gmail.com>
2019-09-17 14:24:36 -0400

    authenication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
+import { decode } from 'he';
+import { withAuth } from '@okta/okta-react';
+import { useAuth } from '/authentication';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+const App = withAuth(({ auth }) => {
+  const [city, GetCity] = useState('');
+  const [authenticated, user] = useAuth(auth);
+
+  const fetchCity = async signal => {
+    const url = new URL('(https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/scores/)');
+    if (user) {
+      url.searchParams.set('firstName', user.given_name);
+      url.searchParams.set('lastName', user.family_name);
+    }
+    const response = await fetch(url, { signal });
+    const { value } = await response.json();
+
+    GetCity(decode(value.city));
+
+
+return (
+
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{city || '...'}</p>
+      <button className="App-link" onClick={() => GetCity('')}>
+        
+      </button>
+      {authenticated !== null && (
+        <button
+          onClick={() => authenticated ? auth.logout() : auth.login()}
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Log {authenticated ? 'out' : 'in'}
+        </button>
+      )}
+    </header>
+  </div>
+);
+
+
 
 export default App;
