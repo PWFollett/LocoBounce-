f9a2475c0bcbb6cd1b33ca8eada821e8848e79e1
PWFollett <pwf592110@gmail.com>
2019-09-17 19:56:23 -0400

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import { Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback, Auth } from '@okta/okta-react';
 import Home from './Home';
 import Protected from './Protected';
+import { createBrowserHistory } from 'history'
+ 
+const history = createBrowserHistory();
+ 
+const auth = new Auth({
+  history,
+  issuer: 'https://{yourOktaDomain}.com/oauth2/default',
+  clientId: '{clientId}',
+  redirectUri: window.location.origin + '/implicit/callback',
+});
  
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Security issuer='https://{yourOktaDomain}.com/oauth2/default'
-                  clientId='{clientId}'
-                  redirectUri={window.location.origin + '/implicit/callback'} >
+      <Router history={history}>
+        <Security auth={auth} >
           <Route path='/' exact={true} component={Home}/>
           <SecureRoute path='/protected' component={Protected}/>
           <Route path='/implicit/callback' component={ImplicitCallback} />
