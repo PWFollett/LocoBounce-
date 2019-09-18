69572bc73685bbb7a24c3e6bd302a7101e15bb41
PWFollett <pwf592110@gmail.com>
2019-09-18 15:25:36 -0400

    removing protection

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, ImplicitCallback, Auth } from '@okta/okta-react';
 import Home from './Home';
-import Protected from './Protected';
 import { createBrowserHistory } from 'history'
  
 const history = createBrowserHistory();
