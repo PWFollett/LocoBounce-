85d30a1e76e19d459910c8d19c73a7e86d1e81ae
PWFollett <pwf592110@gmail.com>
2019-09-18 19:35:52 -0400

    commiting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Card from "./components/Card";
 
-import React, { Component } from 'react';
-import { Router, Route } from 'react-router-dom';
-import { Security, SecureRoute, ImplicitCallback, Auth } from '@okta/okta-react';
-import Home from '/home';
-import Protected from './Protected';
-import { createBrowserHistory } from 'history'
- 
-const history = createBrowserHistory();
- 
-const auth = new Auth({
-  history,
-  issuer: 'https://{outlook-pwf592110.okta.com}.com/oauth2/default',
-  clientId: '{clientId}',
-  redirectUri: window.location.origin + '/implicit/callback',
-});
- 
-class App extends Component {
-  render() {
-    return (
-      <Router history={history}>
-        <Security auth={auth} >
-          <Route path='/' exact={true} component={Home}/>
-          <SecureRoute path='/protected' component={Protected}/>
-          <Route path='/implicit/callback' component={ImplicitCallback} />
-        </Security>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Jumbotron />
+      <Card />
+    </div>
+  );
 }
- 
+
 export default App;
+
